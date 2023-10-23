f921521f00ead288acc59ae9a029a72dbc9add38
lmace-eriks <lucasmace4130@gmail.com>
2023-10-23 14:01:18 -0400

    V1

---
+++ react/Overview.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Link, canUseDOM } from "vtex.render-runtime";
+// @ts-ignore
+import { useProduct } from 'vtex.product-context';
+
+// Styles
+import styles from "./styles.css";
+
+export interface OverviewProps {
+  blockClass: string;
+}
+
+const Overview: StorefrontFunctionComponent<OverviewProps> = ({ }) => {
+  const productContextValue = useProduct();
+  const [overview, setOverview] = useState<string>("");
+
+  useEffect(() => {
+    getOverview();
+  }, []);
+
+  const getOverview = () => {
+    const productInfo = productContextValue.product;
+    console.info(productInfo);
+    if (!productInfo) return;
+
+    const items = productInfo.items;
+    if (!items.length) return;
+
+    const overview = items[0].complementName;
+
+    const overviewHasMarkdown = overview.includes("<div");
+
+    if (overviewHasMarkdown) setOverview(overview);
+  }
+
+  return (<>
+    {
+      overview && <div className={styles.container} dangerouslySetInnerHTML={{ __html: overview }} />
+    }
+  </>);
+};
+
+Overview.schema = {
+  title: "Overview",
+  description: "",
+  type: "object",
+  properties: {
+
+  }
+};
+
+export default Overview;
